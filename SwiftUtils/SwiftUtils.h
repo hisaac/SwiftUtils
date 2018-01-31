26da0946c3c7c34cda5ad785bd493a5bbeca6ec5
Isaac Halvorson <hello@hisaac.net>
2018-01-30 19:13:12 -0600

    Initial Commit

---
+++ SwiftUtils/SwiftUtils.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUtils.h
+//  SwiftUtils
+//
+//  Created by Isaac Halvorson on 1/30/18.
+//  Copyright © 2018 Levelsoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftUtils.
+FOUNDATION_EXPORT double SwiftUtilsVersionNumber;
+
+//! Project version string for SwiftUtils.
+FOUNDATION_EXPORT const unsigned char SwiftUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtils/PublicHeader.h>
+
+
